57d443dc678405dbc356e4964bc905875d3e36f1
Paweł Banach <pawel.banach@capgemini.com>
2016-12-02 17:26:06 +0100

    [WIP] minor changes

--- js/game.js
+++ js/game.js
@@ -1,21 +1,29 @@
-function clickAtButton(cell) {
+function performTurn(cell) {
     document.getElementById(cell.id).innerText = "X";
 };
 
+function cleanUpGrid() {
+    $("#tictactoe-board td").html('');
+    $("#tictactoe-board td").attr('class', '');
+}
+
 
 /* Player module */
-var player = (function () {
+var player = (function() {
     var name = [{
-        playerX: "X", 
+        playerX: "X",
         playerY: "Y"
     }];
 
     return {
-        playerName: function () {
-            return 'Player\'s name is: ' +
-                player.name;
+        playerName: function(data) {
+            if (data === "X") {
+                return player.name.playerX;
+            } else {
+                return player.name.playerY;
+            }
         }
     };
 })();
 
-player.playerName(); // Player's name is: X/Y
+document.getElementById('playerX').innerText = player.playerName(X); // Player's name is: X/Y
