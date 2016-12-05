87c62c51bb4ad61db45436cf6b9176a4a711c21c
Paweł Banach <pawel.banach@capgemini.com>
2016-12-05 16:49:17 +0100

    [WIP] switching starting player

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -1,3 +1,4 @@
+'use strict';
 /*
   0. Init
   1. Clear grid 
@@ -8,41 +9,42 @@
   3. Lock the board 
   4. Set winer / draw
   5. Update History (API)
-  6. New game option
+  6. New game option (In modal)
 */
 
 /** 
  * Variables 
  */
 var cellStates = [null, null, null, null, null, null, null, null, null],
-  cellState = {
-    EMPTY: null,
-    X: 'x',
-    O: 'o'
-  },
-  winPatterns = [
-    [0, 1, 2], // top row
-    [3, 4, 5], // middle row
-    [6, 7, 8], // bottom row
-    [0, 3, 6], // left column
-    [1, 4, 7], // middle column
-    [2, 5, 8], // right column
-    [0, 4, 8], // diagonally
-    [2, 4, 6] // diagonally
-  ], // to win-checker.js
-  player1 = cellState.X,
-  player2 = cellState.O,
-  currentPlayer,
-  gameStates = {
-    IN_PROGRESS: 'in progress',
-    READY: 'ready'
-  },
-  gameState,
-  startingPlayerX = 'First player X',
-  startingPlayerO = 'First player O',
-  startingPlayerButton = document.getElementById('player-switcher'),
-  restartButton = document.getElementById('restart-button'),
-  cells = document.querySelectorAll('.cell');
+    cellState = {
+        EMPTY: null,
+        X: 'x',
+        O: 'o'
+    },
+    winPatterns = [
+        [0, 1, 2], // top row
+        [3, 4, 5], // middle row
+        [6, 7, 8], // bottom row
+        [0, 3, 6], // left column
+        [1, 4, 7], // middle column
+        [2, 5, 8], // right column
+        [0, 4, 8], // diagonally
+        [2, 4, 6] // diagonally
+    ], // to win-checker.js
+    player1 = cellState.X,
+    player2 = cellState.O,
+    currentPlayer,
+    gameStates = {
+        IN_PROGRESS: 'in progress',
+        READY: 'ready',
+        WAITING: 'waiting'
+    },
+    gameState,
+    startingPlayerX = 'First player X',
+    startingPlayerO = 'First player O',
+    startingPlayerButton = document.getElementById('player-switcher'),
+    restartButton = document.getElementById('restart-button'),
+    cells = document.querySelectorAll('.cell');
 
 /**
  * Functions 
@@ -52,12 +54,14 @@ var cellStates = [null, null, null, null, null, null, null, null, null],
  * 0. Init
  */
 function init() {
-  if (!startingPlayerButton.innerText) startingPlayerButton.innerText = startingPlayerX;
 
-  gameState = gameStates.READY;
-  currentPlayer = player1;
+    if (!startingPlayerButton.innerText) startingPlayerButton.innerText = startingPlayerX;
+    gameState = gameStates.READY;
 
-  clearGrid();
+    currentPlayer = player1;
+
+    clearGrid();
+
 }
 
 /**
@@ -65,9 +69,9 @@ function init() {
  */
 function clearGrid() {
 
-  cells.forEach(function (element) {
-    element.innerText = '';
-  }, this);
+    cells.forEach(function(element) {
+        element.innerText = '';
+    }, this);
 
 };
 
@@ -78,21 +82,35 @@ function clearGrid() {
  */
 function switchStartingPlayer() {
 
-  if (gameState !== gameStates.READY) return;
+    if (gameState !== gameStates.READY) return;
 
-  if (startingPlayerButton.innerText === startingPlayerX) {
-    startingPlayerButton.innerText = startingPlayerO;
-    player1 = cellState.O;
-    player2 = cellState.X;
-  } else {
-    startingPlayerButton.innerText = startingPlayerX;
-    player1 = cellState.X;
-    player2 = cellState.O;
-  }
+    if (startingPlayerButton.innerText === startingPlayerX) {
+        startingPlayerButton.innerText = startingPlayerO;
+        player1 = cellState.O;
+        player2 = cellState.X;
+    } else {
+        startingPlayerButton.innerText = startingPlayerX;
+        player1 = cellState.X;
+        player2 = cellState.O;
+    }
 
-  currentPlayer = player1;
+    currentPlayer = player1;
 
 }
+
+if (gameState === gameStates.READY) {
+
+    if (startingPlayerButton.innerText === startingPlayerX) {
+        startingPlayerButton.innerText = startingPlayerO;
+        player1 = cellState.O;
+        player2 = cellState.X;
+    } else {
+        startingPlayerButton.innerText = startingPlayerX;
+        player1 = cellState.X;
+        player2 = cellState.O;
+    }
+
+};
 
 
 /**
@@ -103,48 +121,47 @@ function switchStartingPlayer() {
  */
 function turn(cellId) {
 
-  if (cellStates[cellId] !== cellState.EMPTY) return;
-
-  gameState = gameStates.IN_PROGRESS;
-
-  cellStates[cellId] = currentPlayer; // cellStates Array
-
-  cells[cellId].innerText = currentPlayer; // cells in DOM
-
-  check(currentPlayer);
-
-  currentPlayer = currentPlayer === player1 ? player2 : player1;
+    if (cellStates[cellId] !== cellState.EMPTY) return;
+    if (gameState === gameStates.WAITING) return;
+    gameState = gameStates.IN_PROGRESS;
+    cellStates[cellId] = currentPlayer; // cellStates Array
+    cells[cellId].innerText = currentPlayer; // cells in DOM
+    checkGameStatus(currentPlayer);
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
 
 };
 
 /**
  * 2.2.1. Check if is win/draw
  * 2.2.2.1. Show popup
+ * 4. Set winer / draw
  * 
  * @param {any} currentPlayer
  * @returns
  */
-function check(currentPlayer) { // to winner-checker.js
+function checkGameStatus(currentPlayer) { // to winner-checker.js
 
-  for (var i = 0; i < winPatterns.length; i++) {
+    for (var i = 0; i < winPatterns.length; i++) {
 
-    if ([cellStates[winPatterns[i][0]],
-        cellStates[winPatterns[i][1]],
-        cellStates[winPatterns[i][2]]
-      ]
-      .indexOf(currentPlayer) === -1) {
-      continue;
+        if ([cellStates[winPatterns[i][0]],
+                cellStates[winPatterns[i][1]],
+                cellStates[winPatterns[i][2]]
+            ]
+            .indexOf(currentPlayer) === -1) {
+            continue;
+        }
+
+        if (checkWinPatternsForCurrentPlayer(i, currentPlayer)) { // true if winner is found
+
+            gameState = gameStates.WAITING;
+            cells.disabled = true;
+            alert((currentPlayer + ' won')); // nicer modal maybe?
+
+            return;
+
+        }
+
     }
-    console.log(i);
-    if (checkWinPatternsForCurrentPlayer(i, currentPlayer)) {
-      setTimeout(function () {
-        alert((currentPlayer + " Won"));
-      });
-
-      return;
-    }
-
-  }
 
 }
 
@@ -157,18 +174,28 @@ function check(currentPlayer) { // to winner-checker.js
  */
 function checkWinPatternsForCurrentPlayer(i, currentPlayer) { // to winner-checker.js
 
-  if (cellStates[winPatterns[i][0]] === currentPlayer &&
-    cellStates[winPatterns[i][1]] === currentPlayer &&
-    cellStates[winPatterns[i][2]] === currentPlayer) {
+    if (cellStates[winPatterns[i][0]] === currentPlayer &&
+        cellStates[winPatterns[i][1]] === currentPlayer &&
+        cellStates[winPatterns[i][2]] === currentPlayer) {
 
-    return true;
-  }
+        return true;
+    }
 
-  return false;
+    return false;
 
 }
 
+/**
+ * Reset game function
+ */
+function resetGame() {
 
-(function () {
-  init();
+    cellStates = [null, null, null, null, null, null, null, null, null];
+    cells.disabled = false;
+    init();
+
+}
+
+(function() {
+    init();
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,19 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-gulp.task('copy-css', function () {
-  gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
-    .pipe(gulp.dest('app/styles/'));
+gulp.task('copy-css', function() {
+    gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
+        .pipe(gulp.dest('app/styles/'));
 });
 
-gulp.task('serve', ['copy-css'], function () {
-  browserSync({
-    server: {
-      baseDir: 'app',
-    }
-  });
+gulp.task('serve', ['copy-css'], function() {
+    browserSync({
+        server: {
+            baseDir: 'app',
+        }
+    });
 
-  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {
-    cwd: 'app'
-  }, reload);
+    gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {
+        cwd: 'app'
+    }, reload);
 });
