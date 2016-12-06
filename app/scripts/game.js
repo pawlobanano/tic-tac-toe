d8652fef4a8c00989afda22909d7c0a735fc6d72
Paweł Banach <pawel.banach@capgemini.com>
2016-12-06 11:08:16 +0100

    game module refactored to module pattern; [BUG] cells aren't blocked after game is finished

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -12,122 +12,117 @@
   6. New game option (In modal)
 */
 
-/** 
- * Variables 
- */
-var cellStates = [null, null, null, null, null, null, null, null, null],
-  cellState = {
-    EMPTY: null,
-    X: 'x',
-    O: 'o'
-  },
-  player1 = cellState.X,
-  player2 = cellState.O,
-  firstPlayer = player1,
-  currentPlayer,
-  gameStates = {
-    IN_PROGRESS: 'in progress',
-    READY: 'ready',
-    WAITING: 'waiting'
-  },
-  gameState,
-  gameHasWinner = false,
-  round = 0,
-  startingPlayerX = 'First player X',
-  startingPlayerO = 'First player O',
-  startingPlayerButton = document.getElementById('player-switcher-button'),
-  restartButton = document.getElementById('restart-button'),
-  cells = document.querySelectorAll('.cell');
+var game = (function() {
+    var cellStates = [null, null, null, null, null, null, null, null, null],
+        cellState = {
+            EMPTY: null,
+            X: 'x',
+            O: 'o'
+        },
+        player1 = cellState.X,
+        player2 = cellState.O,
+        firstPlayer = player1,
+        currentPlayer,
+        gameStates = {
+            IN_PROGRESS: 'in progress',
+            READY: 'ready',
+            WAITING: 'waiting'
+        },
+        gameState,
+        gameHasWinner = false,
+        round = 0,
+        startingPlayerX = 'First player X',
+        startingPlayerO = 'First player O',
+        startingPlayerButton = document.getElementById('player-switcher-button'),
+        restartButton = document.getElementById('restart-button'),
+        cells = document.querySelectorAll('.cell'),
+        /**
+         * 1. Clear grid
+         */
+        clearGrid = function() {
+            cells.forEach(function(element) {
+                if (element.innerText === cellState.X) element.classList.remove(cellState.X);
+                if (element.innerText === cellState.O) element.classList.remove(cellState.O);
+                element.innerText = '';
+            }, this);
+        },
+        init = function() {
+            game.init();
+        };
 
-/**
- * Functions 
- */
+    return {
+        /**
+         * 0. Init
+         */
+        init: function() {
+            if (!startingPlayerButton.innerText) startingPlayerButton.innerText = startingPlayerX;
+            gameState = gameStates.READY;
+            currentPlayer = firstPlayer;
+            clearGrid();
+        },
+        /**
+         * 2.1. Perform turn (click on one of grid's cell)
+         * 
+         * @param {any} cellId
+         * @returns
+         */
+        turn: function(cellId) {
+            if (cellStates[cellId] !== cellState.EMPTY) return;
+            if (gameState === gameStates.WAITING) return;
+            gameState = gameStates.IN_PROGRESS;
+            cellStates[cellId] = currentPlayer; // cellStates Array
+            document.getElementById(cellId).classList.add(currentPlayer);
+            cells[cellId].innerText = currentPlayer; // cells in DOM
+            // document.getElementById(cellId).classList.add("animated");// animated appear
+            // document.getElementById(cellId).classList.add("pulse");
+            round++;
+            winnerChecker.checkGameStatus(currentPlayer, cellStates, cellState, round, gameStates, gameState, cells, gameHasWinner);
+            currentPlayer = currentPlayer === player1 ? player2 : player1;
+        },
+        /**
+         * 2. Set sign for starting player (default - X)
+         * 
+         * @returns
+         */
+        switchStartingPlayer: function() {
+            if (gameState !== gameStates.READY) { // gameState === IN_PROGRESS or WAITING
+                if (startingPlayerButton.innerText === startingPlayerX) {
+                    startingPlayerButton.innerText = startingPlayerO;
+                    firstPlayer = cellState.O;
+                } else {
+                    startingPlayerButton.innerText = startingPlayerX;
+                    firstPlayer = cellState.X;
+                }
 
-/**
- * 0. Init
- */
-function init() {
-  if (!startingPlayerButton.innerText) startingPlayerButton.innerText = startingPlayerX;
-  gameState = gameStates.READY;
-  currentPlayer = firstPlayer;
-  clearGrid();
-}
+                return;
+            }
 
-/**
- * 1. Clear grid
- */
-function clearGrid() {
-  cells.forEach(function (element) {
-    if (element.innerText === cellState.X) element.classList.remove(cellState.X);
-    if (element.innerText === cellState.O) element.classList.remove(cellState.O);
-    element.innerText = '';
-  }, this);
-};
+            if (startingPlayerButton.innerText === startingPlayerX) {
+                startingPlayerButton.innerText = startingPlayerO;
+                firstPlayer = cellState.O;
+                player1 = cellState.O;
+                player2 = cellState.X;
+            } else {
+                startingPlayerButton.innerText = startingPlayerX;
+                firstPlayer = cellState.X;
+                player1 = cellState.X;
+                player2 = cellState.O;
+            }
 
-/**
- * 2. Set sign for starting player (default - X)
- * 
- * @returns
- */
-function switchStartingPlayer() {
-  if (gameState !== gameStates.READY) { // gameState === IN_PROGRESS or WAITING
-    if (startingPlayerButton.innerText === startingPlayerX) {
-      startingPlayerButton.innerText = startingPlayerO;
-      firstPlayer = cellState.O;
-    } else {
-      startingPlayerButton.innerText = startingPlayerX;
-      firstPlayer = cellState.X;
+            currentPlayer = player1;
+        },
+        /**
+         * Reset game function
+         */
+        resetGame: function() {
+            cellStates = [null, null, null, null, null, null, null, null, null];
+            cells.disabled = false;
+            round = 0;
+            gameHasWinner = false;
+            init();
+        }
+
     }
-
-    return;
-  }
-
-  if (startingPlayerButton.innerText === startingPlayerX) {
-    startingPlayerButton.innerText = startingPlayerO;
-    firstPlayer = cellState.O;
-    player1 = cellState.O;
-    player2 = cellState.X;
-  } else {
-    startingPlayerButton.innerText = startingPlayerX;
-    firstPlayer = cellState.X;
-    player1 = cellState.X;
-    player2 = cellState.O;
-  }
-
-  currentPlayer = player1;
-}
-
-/**
- * 2.1. Perform turn (click on one of grid's cell)
- * 
- * @param {any} cellId
- * @returns
- */
-function turn(cellId) {
-  if (cellStates[cellId] !== cellState.EMPTY) return;
-  if (gameState === gameStates.WAITING) return;
-  gameState = gameStates.IN_PROGRESS;
-  cellStates[cellId] = currentPlayer; // cellStates Array
-  document.getElementById(cellId).classList.add(currentPlayer);
-  cells[cellId].innerText = currentPlayer; // cells in DOM
-  // document.getElementById(cellId).classList.add("animated");// animated appear
-  // document.getElementById(cellId).classList.add("pulse");
-  round++;
-  winnerChecker.checkGameStatus(currentPlayer);
-  currentPlayer = currentPlayer === player1 ? player2 : player1;
-};
-
-/**
- * Reset game function
- */
-function resetGame() {
-  cellStates = [null, null, null, null, null, null, null, null, null];
-  cells.disabled = false;
-  round = 0;
-  gameHasWinner = false;
-  init();
-}
-
-(function () {
-  init();
 })();
+
+game.init();
--- app/scripts/winner-checker.js
+++ app/scripts/winner-checker.js
@@ -1,76 +1,85 @@
 'use strict';
-var winnerChecker = (function () {
-  var winPatterns = [
-      [0, 1, 2], // top row
-      [3, 4, 5], // middle row
-      [6, 7, 8], // bottom row
-      [0, 3, 6], // left column
-      [1, 4, 7], // middle column
-      [2, 5, 8], // right column
-      [0, 4, 8], // diagonally
-      [2, 4, 6] // diagonally
-    ],
-    /**
-     * Helper function to speed up winPatterns comparison
-     * 
-     * @param {any} i
-     * @param {any} currentPlayer
-     * @returns
-     */
-    checkWinPatternsForCurrentPlayer = function (i, currentPlayer) {
-      if (cellStates[winPatterns[i][0]] === currentPlayer &&
-        cellStates[winPatterns[i][1]] === currentPlayer &&
-        cellStates[winPatterns[i][2]] === currentPlayer) {
-        return true;
-      }
+var winnerChecker = (function() {
+    var winPatterns = [
+            [0, 1, 2], // top row
+            [3, 4, 5], // middle row
+            [6, 7, 8], // bottom row
+            [0, 3, 6], // left column
+            [1, 4, 7], // middle column
+            [2, 5, 8], // right column
+            [0, 4, 8], // diagonally
+            [2, 4, 6] // diagonally
+        ],
+        /**
+         * Helper function to speed up winPatterns comparison
+         * 
+         * @param {any} i
+         * @param {any} currentPlayer
+         * @param {any} cellStates
+         * @returns
+         */
+        checkWinPatternsForCurrentPlayer = function(i, currentPlayer, cellStates) {
+            if (cellStates[winPatterns[i][0]] === currentPlayer &&
+                cellStates[winPatterns[i][1]] === currentPlayer &&
+                cellStates[winPatterns[i][2]] === currentPlayer) {
+                return true;
+            }
 
-      return false;
+            return false;
+        };
+
+    return {
+        /**
+         * 2.2.1. Check if is win/draw
+         * 2.2.2.1. Show popup
+         * 4. Set winer / draw
+         * 
+         * @param {any} currentPlayer
+         * @param {any} cellStates
+         * @param {any} cellState
+         * @param {any} round
+         * @param {any} gameStates
+         * @param {any} gameState
+         * @param {any} cells
+         * @param {any} gameHasWinner
+         * @returns
+         */
+        checkGameStatus: function(currentPlayer, cellStates, cellState, round, gameStates, gameState, cells, gameHasWinner) {
+            for (var i = 0; i < winPatterns.length; i++) { // 8 win patterns for 3x3 grid
+                if ([cellStates[winPatterns[i][0]],
+                        cellStates[winPatterns[i][1]],
+                        cellStates[winPatterns[i][2]]
+                    ]
+                    .indexOf(currentPlayer) === -1) {
+
+                    continue;
+                }
+
+                if (checkWinPatternsForCurrentPlayer(i, currentPlayer, cellStates, cellState)) { // true if winner is found
+                    gameState = gameStates.WAITING;
+                    cells.disabled = true;
+                    gameHasWinner = true;
+
+                    if (currentPlayer === cellState.X) {
+                        historyApi.xWon();
+                        document.getElementById('winsX').innerText = historyApi.getTimesXWon();
+                    } else {
+                        historyApi.oWon();
+                        document.getElementById('winsO').innerText = historyApi.getTimesOWon();
+                    }
+
+                    alert(currentPlayer + ' win!');
+
+                    return;
+                }
+            }
+
+            if (round === 9 && gameHasWinner === false) { // Draw only after all moves
+                cells.disabled = true;
+                historyApi.wasDraw();
+                document.getElementById('draws').innerText = historyApi.getTimesWasDraw();
+                alert('It is draw');
+            };
+        }
     };
-
-  return {
-    /**
-     * 2.2.1. Check if is win/draw
-     * 2.2.2.1. Show popup
-     * 4. Set winer / draw
-     * 
-     * @param {any} currentPlayer
-     * @returns
-     */
-    checkGameStatus: function (currentPlayer) {
-      for (var i = 0; i < winPatterns.length; i++) { // 8 win patterns
-        if ([cellStates[winPatterns[i][0]],
-            cellStates[winPatterns[i][1]],
-            cellStates[winPatterns[i][2]]
-          ]
-          .indexOf(currentPlayer) === -1) {
-
-          continue;
-        }
-
-        if (checkWinPatternsForCurrentPlayer(i, currentPlayer)) { // true if winner is found
-          gameState = gameStates.WAITING;
-          cells.disabled = true;
-          gameHasWinner = true;
-
-          if (currentPlayer === cellState.X) {
-            historyApi.xWon();
-            document.getElementById('winsX').innerText = historyApi.getTimesXWon();
-          } else {
-            historyApi.oWon();
-            document.getElementById('winsO').innerText = historyApi.getTimesOWon();
-          }
-
-          alert(currentPlayer + ' win!');
-          
-          return;
-        }
-      }
-
-      if (round === 9 && gameHasWinner === false) { // Draw only after all moves
-        historyApi.wasDraw();
-        document.getElementById('draws').innerText = historyApi.getTimesWasDraw();
-        alert('It is draw');
-      };
-    }
-  };
 })();
